a9ac2351a1b154474f3b753fe08aeeb0e9313c77
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-03-24 14:50:49 -0600

    modified main.py to loaded the corrected GUI

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from ui import GUI
 def main ():
     # initializing the main application window
     root = tk.TK ()
+    app= GUI(root)
+
+    # sart loop to keep application open 
+    root.mainloop()
+
+if __name__ =="__main__":
+    main()
+
 
