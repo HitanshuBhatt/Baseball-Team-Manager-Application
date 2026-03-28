e4500c43257859f1c24af5789f8428cc09319542
HitanshuBhatt <hitanshusbhatt@gmail.com>
2026-03-28 00:58:43 -0600

    Modified th databse file to link with DB and not the CSV

--- db.py
+++ db.py
@@ -1,23 +1,53 @@
+import os
 import sqlite3
-DB_FILE="players.db"
+
+base_dir = os.path.dirname(__file__)
+DB_FILE = os.path.join(base_dir, "baseball_sql.db")
+
+
+
+
+
 def get_connection():
     return sqlite3.connect(DB_FILE)
+
 def get_player(player_id):
-    # fetch player data for given players id 
-    conn= get_connection()  #connecting to database
-    cursor = conn.cursor() # creartin cursor to execute sql commands 
-    cursor.execute("SELECT * FROM Player WHERE PlayerID=?", (player_id,))
-    player=cursor.fetchone()
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT playerID, batOrder, firstName, lastName, position, atBats, hits FROM Player WHERE playerID=?", 
+                   (player_id,))
+    player = cursor.fetchone()
     conn.close()
     return player
 
-def Update_player(player_id, first_name, last_name, position, at_bats, hits):
-    # updating a player infomation for a spesific id 
+def update_player(player_id, bat_order, first_name, last_name, position, at_bats, hits):
     conn = get_connection()
-    cursor=conn.cursor 
-    sql="""UPDATE Player 
-         set firstName=?, lastName=?, position=?, atbats=?,hits=?
-         WHERE playerid=?"""
-    cursor.excute(sql,(first_name,last_name,position,at_bats,hits, player_id))
+    cursor = conn.cursor()
+    sql = """UPDATE Player
+             SET batOrder=?, firstName=?, lastName=?, position=?, atBats=?, hits=?
+             WHERE playerID=?"""
+    cursor.execute(sql, (bat_order, first_name, last_name, position, at_bats, hits, player_id))
+    conn.commit()
+    conn.close()
+
+
+#  function to add player 
+def add_player(player_id,bat_order,first_name, last_name,position,at_bats,hits):
+    # adding a new player to the database
+    conn = get_connection()
+    cursor=conn.cursor()
+    sql = """INSERT INTO Player 
+             (playerID, batOrder, firstName, lastName, position, atBats, hits) 
+             VALUES (?, ?, ?, ?, ?, ?, ?)"""
+    cursor.execute(sql,(player_id,bat_order,first_name, last_name,position,at_bats,hits))
+    conn.commit() # saving changes in the database
+    conn.close()
+
+def delete_player(player_id):
+    # Deleting a player record based on the playerID
+    conn = get_connection()
+    cursor = conn.cursor()
+    sql = "DELETE FROM Player WHERE playerID = ?"
+    cursor.execute(sql, (player_id,))
     conn.commit()
     conn.close()
